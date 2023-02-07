b6d7468a91621a9929384924ef7105feee7c9479
Jinmyung Kevin Kim <kevkim0201@gmail.com>
2023-02-06 18:13:11 -0800

    Lab Part 1

--- src/modules/iterative/SearchModule.java
+++ src/modules/iterative/SearchModule.java
@@ -9,14 +9,36 @@ public class SearchModule {
 
     //TODO: Complete this search algorithm to find an element location is in array
     //      use while loops here
-    public static int binarySearch(int[] data, int target){
+    public static int binarySearch(int[] data, int target) {
         //TODO: update with search algorithm
-        return 0;
+
+        int min = 0;
+        int max = data.length - 1;
+        int mid;
+
+        while (min <= max) {
+            mid = (min + max) / 2;
+            if (data[mid] == target) {
+                return mid;
+            } else if (data[mid] < target) {
+                min = mid + 1;
+            } else {
+                max = mid - 1;
+            }
+        }
+
+        return -1;
     }
 
 
     //TODO: Complete this search algorithm to check if an element is in array
     public static boolean contains(int[] data, int target) {
+
+        for (int number : data) {
+            if (number == target) {
+                return true;
+            }
+        }
         return false;
     }
 
@@ -24,7 +46,22 @@ public class SearchModule {
     //TODO: Complete this search algorithm to find an element location is in array
     //      use for loops here
     public static int indexOf(int[] data, int target) {
-        return 0;
+
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == target) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 
+
+    public static void main(String[] args){
+        int answer;
+        int[] array = {60, 41, 92, 32, 12, 52, 21};
+        answer = binarySearch(array,41);
+
+        System.out.println(answer);
+    }
 }
--- src/modules/iterative/SortModule.java
+++ src/modules/iterative/SortModule.java
@@ -11,44 +11,88 @@ public class SortModule {
     //      using for loops and swap method (see below)
     public static void bubbleSort(int[] data) {
         //TODO: Complete Body
-    }
 
+
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 1; j < data.length - i; j++) {
+                if (data[j] < data[j-1]){
+                    swap(data, j, j-1);
+                }
+            }
+        }
+    }
 
     //TODO: Update/Complete the following insertionSort algorithm
     //      using for loops
     public static void insertionSort(int[] data) {
         //TODO: Complete Body
+
+        int a;
+        int currentValue;
+
+        for (int i = 1; i < data.length; i++) {
+            currentValue = data[i];
+            for (a = i; a > 0 && currentValue < data[a - 1]; a--) {
+                data[a] = data[a - 1];
+            }
+            data[a] = currentValue;
+        }
     }
 
 
     // This is an example of "finding the run time function"
     // for a maximum value algorithm
-    public static int max(int[] data){
-        int n   = data.length;          // (1)      data size
+
+    /* public static int max(int[] data) {
+        int n = data.length;          // (1)      data size
         int max = data[0];              // (1)
 
         //       (1)
-        for(int i = 0; i < n; i++)     // n times
+        for (int i = 0; i < n; i++) {   // n times
             // (1  + 1)
-            if(max < data[i])               // (1)
+            if (max < data[i])               // (1)
                 max = data[i];              // (1)
-        // (1)      terminates loop
-        return max;                     // (1)
+            // (1)      terminates loop
+        }// (1)
+        return max;
 
-        // run time function f(n) = 1 + 1 + 1 + n(2 + 1 + 1)
-        //                   f(n) = 4n + 5
-    }
+     */
+
+    // run time function f(n) = 1 + 1 + 1 + n(2 + 1 + 1)
+    //                   f(n) = 4n + 5
 
 
     //TODO: Update/Complete the following selectionSort algorithm
     //      using for loops and swap method (see below)
     public static void selectionSort(int[] data) {
         //TODO: Complete Body
-    }
 
+        int minimum;
+        int j;
+        int k;
+
+        for (j = 0; j < data.length; j++) {
+            minimum = j;
+            for (k = j + 1; k < data.length; k++) {
+                if (data[k] < data[minimum]) {
+                    minimum = k;
+                }
+            }
+
+            if (minimum != j) {      /* swapping code */
+                swap(data, minimum, j);
+            }
+        }
+
+    }
 
     //TODO: Update/Complete the following swap method
-    public static void swap(int[] data, int a, int b) {
-        //TODO: Complete Body
+    public static void swap(int[] data, int i, int j) {
+
+        int temp = data[i];
+        data[i] = data[j];
+        data[j] = temp;
+
     }
+
 }
