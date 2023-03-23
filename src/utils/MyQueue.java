4812874722ec0e79bc4ba10629b6a8849d3151c6
Jinmyung Kevin Kim <kevkim0201@gmail.com>
2023-03-22 22:53:37 -0700

    Week 8 Lab (Part 2, 3, 4)

--- src/utils/MyQueue.java
+++ src/utils/MyQueue.java
@@ -1,9 +1,11 @@
 package utils;
 
+import java.util.NoSuchElementException;
+
 /*****************************************************************
  * This class MyQueue implements a FIFO data structure using a
  * Singly Linked List (SLL).
- *
+ * <p>
  * TODO : Please Complete the Following
  *        1. Comment all steps in your methods used for its
  *           implementation. Be clear and precise.
@@ -12,7 +14,105 @@ package utils;
  *        3. Use JavaDoc Comments above each method.
  *****************************************************************/
 
-public class MyQueue {
+public class MyQueue <E> {
 
     //TODO : Complete Body with Data Fields, Methods and Classes
+
+    Node<E> first;
+    Node<E> last;
+
+    int size;
+
+    public MyQueue(){
+        first = null;
+        last = null;
+        size = 0;
+    }
+
+    public boolean add(E item){
+        append(item);
+        size++;
+        return true;
+    }
+
+    private void append(E item){
+        Node<E> oldLast = last;
+
+        Node<E> newNode = new Node<>(item);
+
+        last = newNode;
+
+        if(isEmpty()){
+            first = newNode;
+        }
+        else{
+            oldLast.next = newNode;
+        }
+    }
+
+    private E detach(){
+        Node<E> deletedNode = first;
+        E deletedItem = deletedNode.data;
+        first = first.next;
+        deletedNode.next = null;
+        return deletedItem;
+    }
+
+    public boolean isEmpty(){
+        return size == 0 && first == null;
+    }
+
+    public E peek(){
+        if(isEmpty()){
+            return null;
+        }
+        else{
+            return first.data;
+        }
+    }
+
+    public E remove(){
+        if(isEmpty()){
+            throw new NoSuchElementException();
+        }
+        else{
+            E oldItem = detach();
+            size--;
+            return oldItem;
+        }
+    }
+
+
+    public int size(){
+        return size;
+    }
+
+    public String toString(){
+        if(isEmpty()){
+            return "[]";
+        }
+        else{
+            StringBuilder result = new StringBuilder("[" + first.data);
+            for (Node<E> newNode = first.next; newNode != null; newNode = newNode.next){
+                result.append(", ").append(newNode.data);
+            }
+            return result + "]";
+        }
+    }
+
+    //inner static class
+    public static class Node<E>{
+        E       data;
+        Node<E> next;
+
+        public Node(E data){
+            this(data, null);
+        }
+
+        public Node(E data, Node<E> next){
+            this.data = data;
+            this.next = next;
+        }
+    }
+
 }
--- src/utils/MyStack.java
+++ src/utils/MyStack.java
@@ -1,9 +1,11 @@
 package utils;
 
+import java.util.EmptyStackException;
+
 /*****************************************************************
  * This class MyStack implements a LIFO data structure using a
  * Singly Linked List (SLL).
- *
+ * <p>
  * TODO : Please Complete the Following
  *        1. Comment all steps in your methods used for its
  *           implementation. Be clear and precise.
@@ -12,7 +14,97 @@ package utils;
  *        3. Use JavaDoc Comments above each method.
  *****************************************************************/
 
-public class MyStack {
+public class MyStack <E> {
 
     //TODO : Complete Body with Data Fields, Methods and Classes
+
+    Node<E> first;
+    Node<E> last;
+
+    int     size;
+
+    public MyStack(){
+        first = null;
+        last  = null;
+        size  = 0;
+    }
+
+    private E detach(){
+        Node<E> removeNode = last;
+
+        E deletedItem = removeNode.data;
+        last = last.next;
+        removeNode.next = null;
+        return deletedItem;
+    }
+
+    public boolean isEmpty(){
+        return last == null && size == 0;
+    }
+
+    public E peek(){
+        if (isEmpty()){
+            throw new EmptyStackException();
+        }
+        else{
+            return last.data;
+        }
+    }
+
+    public E pop(){
+        if (isEmpty()){
+            throw new EmptyStackException();
+        }
+
+        E topNode = detach();
+        size--;
+        return topNode;
+    }
+
+
+    public E push(E item){
+        Node<E> newNode = new Node<> (item, last);
+
+        last = newNode;
+
+        if(isEmpty()){
+            first = newNode;
+        }
+
+        size++;
+
+        return last.data;
+
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public String toString(){
+        if (isEmpty()){
+            return "[]";
+        }
+        else{
+            ArrayList<E> list = new ArrayList<>();
+            for (Node<E> node = last; node != null; node = node.next) {
+                list.add(0,node.data);
+            }
+            return list.toString().trim();
+        }
+    }
+
+
+    //static inner class
+
+    public static class Node<E>{
+        E       data;
+        Node<E> next;
+
+        public Node(E data, Node<E> next){
+            this.data = data;
+            this.next = next;
+        }
+    }
+
 }
