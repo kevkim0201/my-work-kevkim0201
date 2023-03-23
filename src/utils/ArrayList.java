f197b64bb2c8e0d18851905a03817674905c034f
Jinmyung Kevin Kim <kevkim0201@gmail.com>
2023-03-22 22:58:54 -0700

    Week 8 Lab (Part 2, 3, 4)

--- src/utils/ArrayList.java
+++ src/utils/ArrayList.java
@@ -1,9 +1,11 @@
 package utils;
 
+import java.util.NoSuchElementException;
+
 /*****************************************************************
  * This class ArrayList implements a contiguous block of data
  * using an array.
- *
+ * <p>
  * TODO : Please Complete the Following
  *        1. Comment all steps in your methods used for its
  *           implementation. Be clear and precise.
@@ -12,7 +14,233 @@ package utils;
  *        3. Use JavaDoc Comments above each method.
  *****************************************************************/
 
-public class ArrayList {
+public class ArrayList <E> implements List<E> {
 
     //TODO : Complete Body with Data Fields, Methods and Classes
+    public static final int DEFAULT_CAPACITY = 10;
+
+    E[] data;
+
+    int size;
+
+    public ArrayList() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    @SuppressWarnings("unchecked")
+    public ArrayList(int capacity) {
+
+        if (capacity < 0) {
+            throw new IllegalArgumentException();
+        } else {
+            data = (E[]) new Object[capacity];
+            size = 0;
+        }
+    }
+
+    public boolean add(E item) {
+        // creating a placeholder for the current size
+        int oldSize = size;
+
+        // allows the size of array to grow if size > length
+        ensureCapacity(size + 1);
+
+        // appends to back of list = first vacant spot
+        data[size] = item;
+
+        // increments size = updated number of occupied spots
+        size++;
+
+        //returns true if size updated by 1
+        return size == (oldSize + 1);
+
+    }
+
+    public void add(int index, E item){
+        if (index > size){
+            throw new IndexOutOfBoundsException();
+        }
+            ensureCapacity(size + 1);
+            shiftRight(index);
+            data[index] = item;
+            size++;
+    }
+
+    public void checkIndex (int index){
+        String message = "Invalid Index:" + index;
+
+        if (index < 0 || index >= size ){
+            throw new IndexOutOfBoundsException(message);
+        }
+    }
+
+    public void clear(){
+        for (int i = 0; i<size; i++){
+            data[i] = null;
+        }
+        size = 0;
+    }
+
+    public boolean contains(E item){
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(item)) {
+                return true;
+            }
+        }
+            return false;
+    }
+
+    @SuppressWarnings("unchecked")
+    public void ensureCapacity(int targetCapacity) {
+        // checks if needed capacity > current array length
+        if (targetCapacity > data.length) {
+
+            // select the larger of the two (double length or target size)
+            int newCapacity = Math.max(targetCapacity, 2 * data.length + 1);
+
+            //make a new array with the larger size
+            E[] newArray = (E[]) new Object[newCapacity];
+
+            //copy the values in to the new array
+            System.arraycopy(data, 0, newArray, 0, data.length);
+
+            // re-assign underlying array reference to new array
+            data = newArray;
+        }
+    }
+
+    public E get(int index){
+        checkIndex(index);
+        return data[index];
+    }
+
+    public int indexOf(E item){
+        for (int i = 0; i < size; i++){
+                if (data[i].equals(item)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+    // returns true if list has no occupied items
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public Iterator<E> iterator(){
+        return new ArrayIterator();
+    }
+
+    public E remove (int index){
+        checkIndex(index);
+        E oldItem = data[index];
+        shiftLeft(index);
+        data[size-1] = null;
+        size--;
+        return oldItem;
+    }
+
+    public boolean remove(E item){
+        for (int i = 0; i < size; i++){
+            if (data[i].equals(item)){
+                data[i] = null;
+                shiftLeft(i);
+                size--;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public E set(int index, E item){
+        checkIndex(index);
+        E oldItem = data[index];
+        data[index] = item;
+        return oldItem;
+    }
+
+
+    private void shiftLeft (int index){
+        for (int i = index; i < size -1; i++){
+            data[i] = data[i+1];
+        }
+    }
+
+    private void shiftRight(int index){
+        for (int i = size; i > index; i--){
+            data[i] = data[i-1];
+        }
+    }
+
+    public int size(){
+        return size;
+    }
+
+    //prints out the values in sequence from 0 to length - 1
+    public String toString(){
+        if(isEmpty()){
+            return "[]";
+        }
+        else {
+            StringBuilder result = new StringBuilder(" [" + data[0]);
+
+            for (int i = 1; i < size; i++) {
+                result.append(", ").append(data[i]);
+            }
+            return result + "]";
+        }
+    }
+
+    private class ArrayIterator implements Iterator<E>{
+
+        int position;
+        boolean isRemovable;
+
+        public ArrayIterator(){
+            position = 0;
+            isRemovable = false;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return position < size;
+        }
+
+        @Override
+        public E next() {
+            String message = "There are no more elements on the left!";
+
+            if (!hasNext()){
+                throw new NoSuchElementException(message);
+            }
+
+            E currentItem = data[position];
+
+            position = position++;
+
+            position++;
+
+            isRemovable = true;
+
+            return currentItem;
+
+        }
+
+        @Override
+        public void remove() {
+            String message = "Cannot remove!";
+
+            if(!isRemovable){
+                throw new IllegalStateException(message);
+            }
+            ArrayList.this.remove(position - 1);
+            isRemovable = false;
+
+        }
+    }
+
 }
+
+
--- src/utils/LinkedList.java
+++ src/utils/LinkedList.java
@@ -1,5 +1,7 @@
 package utils;
 
+import java.util.NoSuchElementException;
+
 /*****************************************************************
  * This class LinkedList implements linked data structures using
  * a Doubly Linked List (DLL).
@@ -12,7 +14,315 @@ package utils;
  *        3. Use JavaDoc Comments above each method.
  *****************************************************************/
 
-public class LinkedList {
+public class LinkedList <E> implements List<E> {
 
     //TODO : Complete Body with Data Fields, Methods and Classes
+
+    Node<E> first;  // reference to first node in list
+
+    Node<E> last;   // reference to last node in list
+
+    int     size;   // counter of index in list
+
+    public LinkedList(){
+        first   = null;
+        last    = null;
+        size    = 0;
+    }
+
+
+    public boolean add(E item){
+        append(item);
+        size++;
+        return true;
+    }
+
+    public void add(int index, E item){
+        if (index == size){
+            append(item);
+        }
+        else{
+            checkIndex(index);
+            insertBefore(index, item);
+        }
+        size++;
+    }
+
+    public void append( E item){
+        Node<E> node = new Node<>(last, item);
+        if (first == null){
+            first = node;
+        }
+        else{
+            last.next = node;
+            node.prev = last;
+        }
+        last = node;
+    }
+
+    private void checkIndex(int index){
+        String message = "Invalid Index";
+
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException(message);
+        }
+    }
+
+
+    public boolean contains(E item){
+        for (Node<E> current = first; current != null; current = current.next){
+            if(current.data.equals(item)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public void clear(){
+        Node<E> node = first;
+        node.next = null;
+        size = 0;
+    }
+
+    private E detach(int index){
+        Node<E> current = first;
+        for (int i = 0; i < index; i++){
+            current = current.next;
+        }
+        if (current == first){
+            first = current.next;
+            current.prev = null;
+        }
+        else if(current == last){
+            last = current.prev;
+            current.next = null;
+        }
+        else{
+            current.prev.next = current.next;
+            current.next.prev = current.prev;
+        }
+        return current.data;
+    }
+
+    public E get(int index){
+        checkIndex(index);
+        Node<E> current;
+
+        if (index < size / 2){
+            current = first;
+            for (int i = 0; i < index; i++){
+                current = current.next;
+            }
+        }
+
+        else{
+            current = last;
+            for (int i = size - 1; i > index; i--){
+                current = current.prev;
+            }
+        }
+        return current.data;
+    }
+
+    public int indexOf(E item){
+        if (last != null && last.data.equals(item)){
+            return size - 1;
+        }
+        Node<E> current = first;
+        int index = 0;
+        for (int i = 0; i < size; i++){
+            if (current.data.equals(item)){
+                return index;
+            }
+            current = current.next;
+            index++;
+        }
+        return -1;
+    }
+
+    private void insertBefore(int index, E item){
+        Node<E> node = new Node<>(last, item, first);
+        if (index == 0){
+            node.next = first;
+            first.prev = node;
+            first = node;
+        }
+        else{
+            Node<E> current = node(index);
+            node.prev = current.prev;
+            node.next = current;
+            current.prev.next = node;
+            current.prev = node;
+        }
+    }
+
+    public boolean isEmpty(){
+        return first == null && size == 0;
+    }
+
+    public Iterator<E> iterator(){
+        return new LinkedIterator();
+    }
+
+    private Node<E> node(int index){
+        Node<E> current;
+        if (index < size / 2){
+            current = first;
+            for (int i = 0; i < index; i++){
+                current = current.next;
+            }
+        }
+        else{
+            current = last;
+            for (int i = size - 1; i > index; i--){
+                current = current.prev;
+            }
+        }
+        return current;
+    }
+
+    public E remove(int index){
+        checkIndex(index);
+        size--;
+        return detach(index);
+    }
+
+    public boolean remove(E item){
+        if (first == null){
+            return false;
+        }
+        Node<E> current;
+
+        for (current = first; current != null; current = current.next){
+            if(current.data.equals(item)){
+                if (current == first){
+                    first = current.next;
+                    if (first != null){
+                        first.prev = null;
+                    }
+                    else{
+                        last = null;
+                    }
+                }
+                else if(current == last){
+                    last = current.prev;
+                    if (last != null){
+                        last.next = null;
+                    }
+                    else{
+                        first = null;
+                    }
+                }
+                else {
+                    current.prev.next = current.next;
+                    current.next.prev = current.prev;
+                }
+                size--;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public E set(int index, E item){
+
+        checkIndex(index);
+        Node<E> current = first;
+        for (int i = 0; i < index; i++){
+            current = current.next;
+        }
+        E newItem = current.data;
+        current.data = item;
+
+        return newItem;
+
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public String toString(){
+        if (isEmpty()) {
+            return "[]";
+        }
+        else {
+            StringBuilder string = new StringBuilder("[" + first.data);
+
+            for (Node<E> node = first.next; node != null; node = node.next){
+                string.append(", ").append(node.data);
+            }
+            return string + "]";
+        }
+    }
+
+
+    private class LinkedIterator implements Iterator<E>{
+
+        int position;
+        Node<E> current;
+        boolean isRemovable;
+
+        public LinkedIterator(){
+            position = 0;
+            current = first;
+            isRemovable = false;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return position < size;
+        }
+
+        @Override
+        public E next() {
+            String message = "There are no more elements on the left!";
+
+            if (!hasNext()){
+                throw new NoSuchElementException(message);
+            }
+
+            E currentItem = current.data;
+            current = current.next;
+            position++;
+            isRemovable = true;
+
+            return currentItem;
+        }
+
+        @Override
+        public void remove() {
+            String message = "Cannot remove!";
+
+            if(!isRemovable){
+                throw new IllegalStateException(message);
+            }
+
+            LinkedList.this.remove(position - 1);
+            isRemovable = false;
+
+        }
+    }
+
+
+    // This is an inner class that is decoupled from outer class.
+    // The purpose is primarily for storage of data, prev and next
+    // reference in linked sequence.
+    private static class Node<E>{
+        E data;             // data storage
+        Node<E> next;       // self-reference after current node
+        Node<E> prev;       // self-reference before current node
+
+        private Node(Node<E> prev, E data){
+            this(prev, data, null);
+        }
+
+        private Node(Node<E> prev, E data, Node<E> next){
+            this.data = data;
+            this.next = next;
+            this.prev = prev;
+        }
+
+    }
 }
